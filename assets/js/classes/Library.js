2b42eb8ded2ec3e8e4e11272c9bdf8f4434f0100
Milyra35 <marie.richir35@gmail.com>
2023-06-19 08:56:39 +0000

    Suite exercice Yepages, intégration et JS

---
+++ assets/js/classes/Library.js
@@ -0,0 +1,77 @@
+import {listBooks} from './data/data-books.js';
+import {Book} from './Book.js';
+
+class Library {
+    #shelf;
+    
+    constructor(shelf) {
+        this.#shelf = [];
+    }
+    
+    get shelf() {
+        return this.#shelf;
+    }
+    set shelf(shelf) {
+        this.#shelf = shelf;
+    }
+    
+    load(bookList) {
+        let newList;
+        for (let i=0, i<bookList.length; i++)
+        {
+            newList = new Book(
+                listBooks[i].title, 
+                listBooks[i].category, 
+                listBooks[i].author, 
+                listBooks[i].resume, 
+                listBooks[i].image);
+        }
+        this.#shelf.push(newList);
+    }
+    
+    addBook(book) {
+        this.#shelf.push(book);
+    }
+    
+    removeBook(book) {
+        for (let i=0; i<this.#shelf.length; i++)
+        {
+            if (this.#shelf[i].title === book.title && this.#shelf[i].author === book.author)
+            {
+                this.#shelf.splice(this.#shelf[i], 1);
+            }
+        }
+    }
+    
+    findBookByTitle(title) {
+        for (let i=0; i<this.#shelf.length; i++)
+        {
+            if (this.#shelf[i].title === title)
+            {
+                return this.#shelf[i];
+            }
+        }
+    }
+    
+    findBookByAuthor(author) {
+        for (let i=0; i<this.#shelf.length; i++)
+        {
+            if (this.#shelf[i].author === author)
+            {
+                return this.#shelf[i];
+            }
+        }
+    }
+    
+    findBookByCategory(category) {
+        for (let i=0; i<this.#shelf.length; i++)
+        {
+            if (this.#shelf[i].category === category)
+            {
+                return this.#shelf[i];
+            }
+        }
+    }
+}
+
+export {Library};
--- assets/js/data/data-books.js
+++ assets/js/data/data-books.js
@@ -1,39 +1,39 @@
-let library = [
+let listBooks = [
     {
         title : "Harry Potter à l'école des sorciers",
         category : "Fantasy jeunesse",
         author : "J.K. Rowling",
         resume : "",
-        image : ""
+        image : "../images/hp1.jpg"
     },
     {
         title : "Cartographie des nuages",
         category : "Science fiction adulte",
         author : "David Mitchell",
         resume : "",
-        image : ""
+        image : "../images/Cloud_atlas.jpg"
     },
     {
         title : "La communauté de l'anneau",
         category : "Fantasy adulte",
         author : "J.R.R. Tolkien",
         resume : "",
-        image : ""
+        image : "../images/lotr.jpg"
     },
     {
         title : "Truth of the divine",
         category : "Science fiction adulte",
         author : "Lindsay Ellis",
         resume : "",
-        image : ""
+        image : "../images/truth-of-the-divine.jpeg"
     },
     {
         title : "La guerre des mondes",
         category : "Science fiction adulte",
         author : "H.G. Wells",
         resume : "",
-        image : ""
+        image : "../images/war-of-the-worlds.jpeg"
     }
 ];
 
-export {library};
+export {listBooks};
