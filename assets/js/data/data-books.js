18e47a3a90c9699153dc1bcc7622462c01025a5f
Milyra35 <marie.richir35@gmail.com>
2023-06-19 07:53:31 +0000

    Début exercice tableau de données

---
+++ assets/js/data/data-books.js
@@ -0,0 +1,39 @@
+let library = [
+    {
+        title : "Harry Potter à l'école des sorciers",
+        category : "Fantasy jeunesse",
+        author : "J.K. Rowling",
+        resume : "",
+        image : ""
+    },
+    {
+        title : "Cartographie des nuages",
+        category : "Science fiction adulte",
+        author : "David Mitchell",
+        resume : "",
+        image : ""
+    },
+    {
+        title : "La communauté de l'anneau",
+        category : "Fantasy adulte",
+        author : "J.R.R. Tolkien",
+        resume : "",
+        image : ""
+    },
+    {
+        title : "Truth of the divine",
+        category : "Science fiction adulte",
+        author : "Lindsay Ellis",
+        resume : "",
+        image : ""
+    },
+    {
+        title : "La guerre des mondes",
+        category : "Science fiction adulte",
+        author : "H.G. Wells",
+        resume : "",
+        image : ""
+    }
+];
+
+export {library};
